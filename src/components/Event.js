ddcd7efab5445de82ec650534fc58a9f2dbf68cb
kensuzuki <kenichi.suzuki.c@jp.ricoh.com>
2019-11-03 15:30:14 +0900

    fixed

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { DELETE_EVENT } from '../actions'
 
 const Event = ({ event, dispatch }) => {
   const id = event.id
   const handleClickDeleteButton = () => {
     const result = window.confirm(`イベント( ${id} )を削除します。よろしいですか？`)
-    if (result) {dispatch({type: 'DELETE_EVENT', id})}
+    if (result) { dispatch({ type: DELETE_EVENT, id }) }
   }
   return (
     <tr>
@@ -12,7 +13,7 @@ const Event = ({ event, dispatch }) => {
       <td>{event.title}</td>
       <td>{event.body}</td>
       <td><button type="button" className="btn btn-danger" onClick={handleClickDeleteButton}>削除</button></td>
-    </tr>              
+    </tr>
   )
 }
 
